94394828f5195c70ebae8aaf7a2d5613d531c5c6
xlfrie <61897082+marvthedonut@users.noreply.github.com>
2023-11-04 10:41:42 -0700

    Finish i3lock

--- lock.sh
+++ lock.sh
@@ -2,4 +2,4 @@
 
 
 #./lockscreen-gen.sh
-i3lock --indicator --line-color=ffffff00 --ring-color=ffffff4f --inside-color=ffffff1e --keyhl-color=00ff004f --bshl-color=ff00004f --separator-color=00000000 --ringwrong-color=ff00004f --insidewrong-color=ff00001e --ringver-color=0000ff-4f --insidever-color=0000ff1e -i config/wallpapers/lockscreen.png
+i3lock --indicator --line-color=ffffff00 --ring-color=ffffff4f --inside-color=ffffff1e --keyhl-color=00ff004f --bshl-color=ff00004f --separator-color=00000000 --ringwrong-color=ff00004f --insidewrong-color=ff00001e --ringver-color=0000ff4f --insidever-color=0000ff1e -i ~/Downloads/lockscreen.png
