b19e1791c40688d6d2be7f40ac2edabc8744846b
xlfrie <61897082+marvthedonut@users.noreply.github.com>
2023-11-04 18:28:07 -0700

    Add some plugins

--- config/neovim/lua/pluginlist.lua
+++ config/neovim/lua/pluginlist.lua
@@ -1,1 +1,35 @@
-return {}
+return {
+    {
+	"numToStr/Comment.nvim",
+	config = function()
+	    require("Comment").setup()
+	end
+    },
+    {
+	"navarasu/onedark.nvim",
+	priority=1000,
+	config = function()
+	    require("onedark").setup {
+		transparent = true,
+	    }
+	    vim.cmd("colorscheme onedark")
+	end
+    },
+    {
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+		"nvim-tree/nvim-web-devicons"
+		},
+		config = function()
+			require("lualine").setup({
+				icons_enabled = true,
+				theme = "onedark",
+			})
+		end
+	},
+	{
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	}
+}	
