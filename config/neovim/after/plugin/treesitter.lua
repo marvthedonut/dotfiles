c607a4b0c2b1295f3e71a17e2a71ae81163ce05a
marvthedonut <xlfrys@gmail.com>
2023-11-04 19:36:22 -0700

    Revert "Merge branches"

--- config/neovim/after/plugin/treesitter.lua
+++
@@ -1,7 +0,0 @@
-require('nvim-treesitter.configs').setup {
-	ensure_installed = { "vim", 'vimdoc', 'lua', 'cpp', 'python', "bash" },
-	auto_install = false,
-	highlight = { enable = true },
-	indent = { enable = true },
-}
-
--- config/neovim/lua/options.lua
+++
@@ -1,6 +0,0 @@
-vim.o.mouse = "a"
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.number = true
