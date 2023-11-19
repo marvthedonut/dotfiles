17487f18e15ea333c382657f905e98615aad3a76
marvthedonut <61897082+marvthedonut@users.noreply.github.com>
2023-11-19 13:20:32 -0800

    Remove sudo in mkdir

--- install.py
+++ install.py
@@ -40,7 +40,7 @@ def make_dirs(symlinks):
         path = os.path.dirname(link)
         if not os.path.exists(path):
             print(f"Making path {path}")
-            os.system(f"sudo mkdir -p {path}")
+            os.system(f"mkdir -p {path}")
 
 
 
