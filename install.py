269afd1bbee2ea9a60359383f24ae453701e457a
marvthedonut <61897082+marvthedonut@users.noreply.github.com>
2023-11-19 15:55:05 -0800

    Remove some os.system

--- install.py
+++ install.py
@@ -40,8 +40,8 @@ def make_dirs(symlinks):
         path = os.path.dirname(link)
         if not os.path.exists(path):
             print(f"Making path {path}")
-            os.system(f"sudo mkdir -p {path}")
-            os.system(f"sudo chown -R \"$USER:\" {path}")
+            subprocess.call(["sudo" "mkdir" "-p" "path"])
+            subprocess.call(["sudo", "chown", "-R", "\"$USER:\"", path])
 
 
 
