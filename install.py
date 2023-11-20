909f00d795f00a317f52a1e2b2c29e2886e33182
marvthedonut <61897082+marvthedonut@users.noreply.github.com>
2023-11-19 16:27:30 -0800

    Fix for mkdir timings

--- install.py
+++ install.py
@@ -40,8 +40,8 @@ def make_dirs(symlinks):
         path = os.path.dirname(link)
         if not os.path.exists(path):
             print(f"Making path {path}")
-            subprocess.call(["sudo", "mkdir", "-p", path])
-            subprocess.call(["sudo", "chown", "-R", os.environ.get("USER") + ":", path])
+            subprocess.call(["sudo", "mkdir", "-p", path, "&&", "sudo", "chown", "-R", os.environ.get("USER") + ":", path ])
+            #subprocess.call(["sudo", "chown", "-R", os.environ.get("USER") + ":", path])
 
 
 
