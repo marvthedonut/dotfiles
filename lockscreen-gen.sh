b384012a571ae89f9bca28a54ec0bc12cb0e2214
marvthedonut <61897082+marvthedonut@users.noreply.github.com>
2023-12-08 22:10:38 -0800

    Update lockscreen image

--- lockscreen-gen.sh
+++ lockscreen-gen.sh
@@ -4,7 +4,7 @@
 RES=$(xdpyinfo | grep dimensions | cut -d\  -f7)
 RES_COLON=$(sed s/x/:/g<<<"$RES")
 LOCKSCREEN="lockscreen.png"
-WALLPAPER="yxuve5xdpmq91.png"
+WALLPAPER="jwst-wallpaper-cropped.png"
 
 echo Got res $RES_COLON
 
