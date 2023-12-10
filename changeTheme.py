087cc392c5d93059507ec9883a9fc0e9c44ed77e
marvthedonut <61897082+marvthedonut@users.noreply.github.com>
2023-12-10 12:35:21 -0800

    Add method to change themes

---
+++ changeTheme.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+
+def main(args):
+    if len(args) <= 1:
+        print("Pass in wallpaper to change to")
+        return
+    wallpaperPath = os.path.abspath("config/wallpapers/" + args[1] + ".png")
+    colorsPath = os.path.abspath("config/polybar/colors/" + args[1] + ".ini")
+    if not os.path.exists(wallpaperPath) or "lockscreen.png" == args[1]:
+        print("Not a wallpaper")
+        return
+
+    if os.path.exists(os.path.expanduser("~/Downloads/wallpaper.png")):
+        print("Removing existng wallpaper")
+        os.remove(os.path.expanduser("~/Downloads/wallpaper.png"))
+    if os.path.exists("/etc/polybar/colors.ini"):
+        print("Removing colors symlink")
+        os.remove("/etc/polybar/colors.ini")
+
+    os.system(f"sudo ln -s {wallpaperPath} ~/Downloads/wallpaper.png")
+    os.system(f"sudo ln -s {colorsPath} /etc/polybar/colors.ini")
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
