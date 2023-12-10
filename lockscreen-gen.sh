75ff650ddd765ac3ef171445359b03c7f160ad76
marvthedonut <61897082+marvthedonut@users.noreply.github.com>
2023-12-10 15:28:20 -0800

    Undo lock gen clear

--- lockscreen-gen.sh
+++ lockscreen-gen.sh
@@ -12,5 +12,4 @@ cd config/wallpapers
 rm $LOCKSCREEN
 
 ffmpeg -i $WALLPAPER -vf "scale=$RES_COLON,boxblur=8" $LOCKSCREEN
-clear
 convert $LOCKSCREEN -font JetBrainsMono-NF-Bold -family "JetbrainsMono Nerd Font" -fill white -gravity center -pointsize 26 -annotate +0+125 'Type password to unlock' $LOCKSCREEN
